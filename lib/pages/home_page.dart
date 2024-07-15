fd093520c502108c088dd66e79bca372a6774896
unknown <kemdjoarthur@gmail.com>
2024-07-15 09:43:54 +0200

    staged from Arthur (facutatif)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
-
-
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  static List<Widget> _widgetOptions = <Widget>[
-    DashboardScreen(),
-    SearchScreen(),
-    ControlScreen(),
-    SettingsScreen(),
-  ];
+class _MyAppState extends State<MyApp> {
+  bool isDarkTheme = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EcoWatt',
+      theme: isDarkTheme ? ThemeData.dark() : ThemeData.light(),
+      home: HomeScreen(
+        onThemeChanged: (value) {
+          setState(() {
+            isDarkTheme = value;
+          });
+        },
+        isDarkTheme: isDarkTheme,
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  final ValueChanged<bool> onThemeChanged;
+  final bool isDarkTheme;
+
+  HomeScreen({required this.onThemeChanged, required this.isDarkTheme});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -23,13 +49,21 @@ class _MainScreenState extends State<MainScreen> {
     });
   }
 
+  static List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    SearchScreen(),
+    ControlScreen(),
+    SettingsScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Accueil',
@@ -48,147 +82,155 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
+        selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
       ),
     );
   }
 }
 
-class DashboardScreen extends StatefulWidget {
-  @override
-  _DashboardScreenState createState() => _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
-        title: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/profile_image.png'),
-              ),
-              SizedBox(width: 10),
-              Text(
-                'Hello, Jane Doe',
-                style: TextStyle(
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Spacer(),
-              Icon(
-                Icons.notifications,
-                color: Colors.black,
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Today's, Week's, and Month's consumption
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text.rich(
+              TextSpan(
+                text: 'Hello, ',
                 children: [
-                  _buildConsumptionCard('Aujourd\'hui', '132.92 kWh', Colors.red),
-                  _buildConsumptionCard('Semaine', '74.92 kWh', Colors.blue),
-                  _buildConsumptionCard('Mois', '564.92 kWh', Colors.orange),
+                  TextSpan(
+                    text: 'Jane Doe',
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.orange),
+                  ),
                 ],
               ),
-              SizedBox(height: 20),
-              // Set a new limit section
-              _buildLimitCard(),
-              SizedBox(height: 20),
-              // Devices section
-              _buildDevicesCard(),
-              SizedBox(height: 20),
-              // Settings section
-              _buildSettingsCard(),
-              SizedBox(height: 20),
-              // Consumption per device and Theme settings
-              _buildConsumptionAndThemeCard(),
-            ],
+            ),
+            CircleAvatar(
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Handle notifications
+            },
           ),
-        ),
+        ],
       ),
-    );
-  }
-
-  Widget _buildConsumptionCard(String title, String consumption, Color color) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 4.0),
-        padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: color.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(12.0),
-        ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildEnergyConsumptionCard('Aujourd\'hui', '17,92 kWh', Colors.orange),
+                buildEnergyConsumptionCard('Semaine', '74,92 kWh', Colors.blue),
+                buildEnergyConsumptionCard('Mois', '564,92 kWh', Colors.red),
+              ],
             ),
-            SizedBox(height: 8),
-            Text(
-              consumption,
-              style: TextStyle(
-                color: color,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-            ),
+            SizedBox(height: 16.0),
+            buildProgressCard(),
+            SizedBox(height: 16.0),
+            buildWelcomeCard(),
+            SizedBox(height: 16.0),
+            buildDeviceControlCard(),
+            SizedBox(height: 16.0),
+            buildSchedulerCard(),
+            SizedBox(height: 16.0),
+            buildNotificationsCard(),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildLimitCard() {
+  Widget buildEnergyConsumptionCard(String title, String energy, Color color) {
     return Container(
-      padding: const EdgeInsets.all(16.0),
+      width: 100,
+      padding: EdgeInsets.all(8.0),
       decoration: BoxDecoration(
-        color: Colors.orange.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(12.0),
+        color: color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8.0),
       ),
       child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '55 %',
-                style: TextStyle(
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-              ),
-              Text('Limit reached'),
-            ],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 8),
+          SizedBox(height: 8.0),
+          Text(
+            energy,
+            style: TextStyle(fontSize: 14.0),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProgressCard() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.orange.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          CircularPercentIndicator(
+            radius: 60.0,
+            lineWidth: 8.0,
+            percent: 0.55,
+            center: Text('55%'),
+            progressColor: Colors.orange,
+          ),
+          Text(
+            'Définir une limite',
+            style: TextStyle(fontSize: 16.0, color: Colors.orange),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWelcomeCard() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.lightBlue.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Bienvenue dans ECOWATT',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'Ajouter des appareils connectés et gérez votre consommation d\'énergie de manière efficace.',
+            style: TextStyle(fontSize: 14.0),
+          ),
+          SizedBox(height: 8.0),
           ElevatedButton(
-            onPressed: () {},
-            child: Text('Définir une limite'),
+            onPressed: () {
+              // Handle add device action
+            },
+            child: Text('Ajouter un appareil'),
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.orange,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
+              primary: Colors.blue,
+              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+              textStyle: TextStyle(fontSize: 16),
             ),
           ),
         ],
@@ -196,49 +238,62 @@ class _DashboardScreenState extends State<DashboardScreen> {
     );
   }
 
-  Widget _buildDevicesCard() {
+  Widget buildDeviceControlCard() {
     return Container(
-      padding: const EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
         color: Colors.blue.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(12.0),
+        borderRadius: BorderRadius.circular(8.0),
       ),
       child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildDeviceCard(Icons.lightbulb_outline, 'Light Bulb'),
-              _buildDeviceCard(Icons.ac_unit, 'Air Con.'),
-              _buildDeviceCard(Icons.tv, 'Television'),
-            ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Appareil actuel: Light Bulb 50Wh',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 8.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Energy Consumed'),
-              Text(
-                '74.92 kWh',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  // Handle change color action
+                },
+                child: Text('Modifier la couleur'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  textStyle: TextStyle(fontSize: 14),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle change brightness action
+                },
+                child: Text('Modifier la luminosité'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  textStyle: TextStyle(fontSize: 14),
                 ),
               ),
             ],
           ),
-          SizedBox(height: 8),
+          SizedBox(height: 8.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Energy Saved'),
-              Text(
-                '22.38 kWh',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            children: <Widget>[
+              Text('Énergie consommée', style: TextStyle(fontSize: 14.0)),
+              Text('74.92 kWh', style: TextStyle(fontSize: 14.0)),
+            ],
+          ),
+          SizedBox(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Énergie économisée', style: TextStyle(fontSize: 14.0)),
+              Text('22.38 kWh', style: TextStyle(fontSize: 14.0)),
             ],
           ),
         ],
@@ -246,73 +301,67 @@ class _DashboardScreenState extends State<DashboardScreen> {
     );
   }
 
-  Widget _buildDeviceCard(IconData icon, String label) {
-    return Column(
-      children: [
-        Icon(
-          icon,
-          color: Colors.blue,
-          size: 30,
-        ),
-        SizedBox(height: 8),
-        Text(label),
-      ],
-    );
-  }
-
-  Widget _buildSettingsCard() {
+  Widget buildSchedulerCard() {
     return Container(
-      padding: const EdgeInsets.only(top: 16.0,bottom: 16),
+      padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        color: Colors.blue.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.black.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8.0),
       ),
       child: Column(
-        children: [
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Planifier vos horaires',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildSettingSlider('Vibrancy'),
-              _buildSettingSlider('Luminosity'),
+            children: <Widget>[
+              Text('De', style: TextStyle(fontSize: 14.0)),
+              Text('18:00', style: TextStyle(fontSize: 14.0)),
+              Text('À', style: TextStyle(fontSize: 14.0)),
+              Text('17:00', style: TextStyle(fontSize: 14.0)),
+              Text('Période', style: TextStyle(fontSize: 14.0)),
+              Text('Lundi', style: TextStyle(fontSize: 14.0)),
+              Text('Statut', style: TextStyle(fontSize: 14.0)),
+              Text('Actif', style: TextStyle(fontSize: 14.0)),
             ],
           ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Usage Summary'),
-              Text('74.92 kWh'),
-            ],
+          SizedBox(height: 8.0),
+          ElevatedButton(
+            onPressed: () {
+              // Handle schedule action
+            },
+            child: Text('Planifier'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.black,
+              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+              textStyle: TextStyle(fontSize: 16),
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildSettingSlider(String label) {
-    return Column(
-      children: [
-        Text(label),
-        Slider(
-          value: 0.4,
-          onChanged: (value) {},
-        ),
-      ],
-    );
-  }
-
-  Widget _buildConsumptionAndThemeCard() {
+  Widget buildNotificationsCard() {
     return Container(
-      padding: const EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        color: Colors.yellow.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.orange.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8.0),
       ),
       child: Column(
-        children: [
-          Text('Consommation par appareil'),
-          SizedBox(height: 20),
-          Text('Paramètres du thème'),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Notifications',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          // Add notification content here
         ],
       ),
     );
@@ -323,10 +372,11 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Recherche Screen'),
-        ),
+      appBar: AppBar(
+        title: Text('Recherche'),
+      ),
+      body: Center(
+        child: Text('Recherche'),
       ),
     );
   }
@@ -335,20 +385,46 @@ class SearchScreen extends StatelessWidget {
 class ControlScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Control Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Control'),
+      ),
+      body: Center(
+        child: Text('Control'),
+      ),
     );
   }
 }
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  final ValueChanged<bool> onThemeChanged;
+  final bool isDarkTheme;
+
+  SettingsScreen({required this.onThemeChanged, required this.isDarkTheme});
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Paramètres Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Paramètres'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Paramètres'),
+            Switch(
+              value: widget.isDarkTheme,
+              onChanged: widget.onThemeChanged,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
-
